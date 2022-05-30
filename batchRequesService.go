82b0420065439b0f2d02f961ec762e9db5b00026
Nikita <karneges>
2022-05-30 16:26:44 +0300

    first

---
+++ batchRequesService.go
@@ -0,0 +1,70 @@
+package batchRequestService
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/gagliardetto/solana-go/rpc/jsonrpc"
+	"github.com/samber/lo"
+)
+
+type BatchRequestService struct {
+	client jsonrpc.RPCClient
+}
+
+func New(endPoint string) *BatchRequestService {
+	return &BatchRequestService{
+		client: jsonrpc.NewClient(endPoint),
+	}
+}
+
+type BatchRequester interface {
+	GetSignaturesForAddresses(accounts []solana.PublicKey) (map[solana.PublicKey][]*rpc.TransactionSignature, error)
+	GetTransactions(signatures []solana.Signature) (map[solana.Signature]*rpc.TransactionWithMeta, error)
+}
+
+func (b *BatchRequestService) GetSignaturesForAddresses(accounts []solana.PublicKey) (map[solana.PublicKey][]*rpc.TransactionSignature, error) {
+	outMap := make(map[solana.PublicKey][]*rpc.TransactionSignature)
+	batch, err := b.client.CallBatch(context.TODO(), lo.Map(accounts, func(t solana.PublicKey, i int) *jsonrpc.RPCRequest {
+		return jsonrpc.NewRequest("getSignaturesForAddress", []interface{}{t, rpc.M{"commitment": rpc.CommitmentConfirmed, "limit": 15}})
+	}),
+	)
+	if err != nil {
+		return outMap, err
+	}
+	lo.ForEach(batch, func(t *jsonrpc.RPCResponse, i int) {
+		var out []*rpc.TransactionSignature
+		err := t.GetObject(&out)
+		if err != nil {
+			panic(err)
+		}
+		outMap[accounts[t.ID]] = out
+	})
+	return outMap, nil
+}
+
+func (b *BatchRequestService) GetTransactions(signatures []solana.Signature) (map[solana.Signature]*rpc.TransactionWithMeta, error) {
+
+	outMap := make(map[solana.Signature]*rpc.TransactionWithMeta)
+	batch, err := b.client.CallBatch(context.TODO(), lo.Map(signatures, func(t solana.Signature, i int) *jsonrpc.RPCRequest {
+		return jsonrpc.NewRequest("getConfirmedTransaction", []interface{}{t, rpc.M{"commitment": rpc.CommitmentConfirmed}})
+	}),
+	)
+	if err != nil {
+		return outMap, err
+	}
+	if len(signatures) != len(batch) {
+		return outMap, errors.New(fmt.Sprintf("Batch input/output mismatch %v vs %v", len(signatures), len(batch)))
+	}
+	lo.ForEach(batch, func(t *jsonrpc.RPCResponse, i int) {
+		var out *rpc.TransactionWithMeta
+		err := t.GetObject(&out)
+		if err != nil {
+			return
+		}
+		outMap[signatures[t.ID]] = out
+	})
+	return outMap, nil
+}
